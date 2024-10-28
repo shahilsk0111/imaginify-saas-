8dac8801e4721d2a78eec2c0ed57afccffd26b7c
shahil <shahilsk0111@gmail.com>
2024-10-28 12:25:51 +0530

    initial commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+     <p>home</p>
+
+     
+    </div>
+  )
+}
+
+export default Home
---
+++ app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TrasnformationsPage = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default TrasnformationsPage
